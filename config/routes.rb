79742a2dcb4c9b6908462fb1a461d089451ef113
Nico <nicovigil1@gmail.com>
2019-02-12 16:52:11 -0700

    adds path_helper to welcome#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: "welcome#index"
+  get '/', to: "welcome#index", as: 'home'
 
   get '/auth/spotify/callback', to: 'sessions#create'
 end
