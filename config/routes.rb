6d6a74ba1c391f4d6b28e5614b086b369e90fa85
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-13 13:52:55 -0700

    Adds dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/', to: "welcome#index", as: 'home'
 
   get '/auth/spotify/callback', to: 'sessions#create'
+
+  get '/dashboard', to: "users#show", as: "dashboard"
 end
