ad9c3bf51e5d1106bbcfaef413af83513a4735fc
Nico <nicovigil1@gmail.com>
2019-02-13 22:26:26 -0700

    adds refresh token to user upon creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
     user_info = spotify_params(request.env['omniauth.auth'].credentials,
                         request.env['omniauth.auth'].info)
     user = User.create(user_info)
-    require 'pry'; binding.pry
     session[:user_id] = user.id
     flash[:success] = "Successfully signed in."
     redirect_to dashboard_path
@@ -16,7 +15,7 @@ class SessionsController < ApplicationController
     {username: profile["nickname"],
      image_url: profile["image"],
      spotify_token: credentials["token"],
-     refresh_token: credentials["refresh_token"]
+     refresh_token: credentials["refresh_token"],
      profile_url: profile["urls"]["spotify"]}
   end
 
