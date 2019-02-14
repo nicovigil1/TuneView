e8c815179b3da872a3c4706c1ab24e6a34d5404c
Nico <nicovigil1@gmail.com>
2019-02-13 17:26:00 -0700

    edits oauth scope, adds faraday middleware for automatic response parsing

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'omniauth'
 gem 'omniauth-spotify'
 gem 'semantic-ui-sass'
+gem 'faraday_middleware'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: %w(
+    user-top-read
+    user-read-recently-played
     playlist-read-private
     user-read-private
     user-read-email
