764d27bc83c102f7b5e77c1efa0559f48b352b84
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-14 11:18:56 -0700

    Adds most recent played API call to spotify service

--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -3,16 +3,20 @@ class SpotifyService
     Faraday.new(url: "https://api.spotify.com") do |faraday|
       faraday.request :url_encoded
       faraday.adapter Faraday.default_adapter
-      faraday.headers["Authorization"] = "Bearer #{current_user.spotify_token}" 
+      faraday.headers["Authorization"] = "Bearer #{current_user.spotify_token}"
       faraday.response :json, :parser_options => { :symbolize_names => true }
     end
-  end 
+  end
 
   def self.user_data(user)
     conn(user).get("/v1/me/").body
-  end 
+  end
 
   def self.top_5_artists(user)
     conn(user).get("/v1/me/top/artists?limit=5")
-  end 
+  end
+
+  def self.most_recent_track(user)
+    conn(user).get("/v1/me/player/recently-played?type=track&limit=1")
+  end
 end
