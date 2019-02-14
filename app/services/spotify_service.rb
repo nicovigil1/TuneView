30f588e606178996fd56405a81d738bf2437e1a1
Nico <nicovigil1@gmail.com>
2019-02-13 18:58:49 -0700

    adds minor fixes to conn class in spotify service

--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -1,14 +1,18 @@
 class SpotifyService
-  def self.conn
+  def self.conn(current_user)
     Faraday.new(url: "https://api.spotify.com") do |faraday|
       faraday.request :url_encoded
       faraday.adapter Faraday.default_adapter
-      faraday.headers["Authorization"] = "Bearer #{ENV['S_TEST_TOKEN']}" 
+      faraday.headers["Authorization"] = "Bearer #{current_user.spotify_token}" 
       faraday.response :json, :parser_options => { :symbolize_names => true }
     end
   end 
 
   def self.user_data(user)
-    conn.get("/v1/me/").body
+    conn(user).get("/v1/me/").body
+  end 
+
+  def self.top_5_artists(user)
+    conn(user).get("/v1/me/top/artists?limit=5")
   end 
 end
--- spec/services/spotify_service_spec.rb
+++ spec/services/spotify_service_spec.rb
@@ -7,10 +7,10 @@ describe SpotifyService do
               image_url: "https://bit.ly/2tlLmZc", 
               spotify_token: ENV["S_TEST_TOKEN"], 
               profile_url: "https://open.spotify.com/user/12184696969"}
-
       current_user = User.create(info)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user)
       response = SpotifyService.user_data(current_user)
-      
+
       expect(response[:email]).to be_truthy 
     end
     it 'can find the top 5 artists per user' do 
@@ -22,7 +22,7 @@ describe SpotifyService do
       current_user = User.create(info)
       
       response = SpotifyService.top_5_artists(current_user)
-
+      require 'pry'; binding.pry
       expect(response.length).to eq(5)
     end 
   end
