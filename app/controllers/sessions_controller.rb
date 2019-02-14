26eb771ca3f66a0f990fdc709fbacfbb4f69b1a8
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-14 11:19:57 -0700

    Integrates variables into dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
   def create
     user_info = spotify_params(request.env['omniauth.auth'].credentials,
-                        request.env['omniauth.auth'].info)
+                               request.env['omniauth.auth'].info
+                             )
     user = User.create(user_info)
     session[:user_id] = user.id
-    flash[:success] = "Successfully signed in."
+    flash[:success] = "Successfully signed in." if user.save
     redirect_to dashboard_path
   end
 
@@ -12,7 +13,7 @@ class SessionsController < ApplicationController
 
   def spotify_params(credentials, profile)
     profile["image"] ||= "https://bit.ly/2tlLmZc"
-    {username: profile["nickname"],
+    {username: profile["name"],
      image_url: profile["image"],
      spotify_token: credentials["token"],
      profile_url: profile["urls"]["spotify"]}
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
   def show
+    recent_track = SpotifyService.most_recent_track(current_user)
+    @title = recent_track.body[:items][0][:track][:name]
+    @artist = recent_track.body[:items][0][:track][:artists][0][:name]
+    @album = recent_track.body[:items].first[:track][:album][:name]
+    @cover_url = recent_track.body[:items].first[:track][:album][:images][1][:url]
   end
 end
