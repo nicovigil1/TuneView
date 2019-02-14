daf9f79c39b3dfab30b65bf8cb85db861ad70ebe
Nico <nicovigil1@gmail.com>
2019-02-13 18:46:48 -0700

    adds poro test for SpotifyService

--- spec/services/spotify_service_spec.rb
+++ spec/services/spotify_service_spec.rb
@@ -13,5 +13,17 @@ describe SpotifyService do
       
       expect(response[:email]).to be_truthy 
     end
+    it 'can find the top 5 artists per user' do 
+      info = {username: "12184696969", 
+        image_url: "https://bit.ly/2tlLmZc", 
+        spotify_token: ENV["S_TEST_TOKEN"], 
+        profile_url: "https://open.spotify.com/user/12184696969"}
+
+      current_user = User.create(info)
+      
+      response = SpotifyService.top_5_artists(current_user)
+
+      expect(response.length).to eq(5)
+    end 
   end
 end
