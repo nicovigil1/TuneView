b15fdb132ed7cd8d0a83f8b595dab65f5dbd0234
Nico <nicovigil1@gmail.com>
2019-02-16 11:49:10 -0700

    adds expired token filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-
+  before_action :expired_filter
   helper_method :current_user
 
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
+
+  def expired_filter
+    if SpotifyService.user_data(current_user)[:error]
+      SpotifyService.refresh_token(current_user)
+    else 
+      nil
+    end 
+  end 
 end
