92d1ea1613a96853abd8c63ad06958191aa0ef5e
Nico <nicovigil1@gmail.com>
2019-02-16 10:07:48 -0700

    moves tests and files around for user facade to take on more responsibilty

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -11,10 +11,10 @@ class Artist
     @uid = data[:id]
   end 
 
-  def self.top_5(user)
-    artist_data = SpotifyService.top_5_artists(user)
-    artist_data.map do |data|
-      Artist.new(data)
-    end
-  end 
+  # def self.top_5(user)
+  #   artist_data = SpotifyService.top_5_artists(user)
+  #   artist_data.map do |data|
+  #     Artist.new(data)
+  #   end
+  # end 
 end 
