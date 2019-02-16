39db1d843eb538dc41fda9e8f835a36cea993ffa
Nico <nicovigil1@gmail.com>
2019-02-16 07:22:34 -0700

    adds functioning discogs service (search function)

---
+++ app/services/discogs_service.rb
@@ -0,0 +1,16 @@
+class DiscogsService
+  def self.conn
+    Faraday.new(url: "https://api.discogs.com") do |conn|
+      conn.request :url_encoded
+      conn.adapter Faraday.default_adapter
+      conn.response :json, :parser_options => {symbolize_names: true}
+      conn.params["key"] = ENV["DISCOGS_KEY"]
+      conn.params["secret"] = ENV["DISCOGS_SECRET"]
+    end
+  end 
+
+  def self.search(query, type)
+    params = "{#{query}}&{?#{type}}&{release}&{?type}"
+    conn.get("database/search?q=#{params}")
+  end
+end 
