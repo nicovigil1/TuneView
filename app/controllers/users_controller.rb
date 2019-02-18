c1d05e3fed12d370286eb5c079ce45ddd6f92048
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-18 12:42:33 -0700

    Adds messages to dashboard controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,10 @@ class UsersController < ApplicationController
   def show
     # SpotifyService.refresh_token(current_user)  #if token_expired?
     @top_5_artists = user_info.top_5_artists
-    @top_5_tracks 
+    @top_5_tracks
     @most_recent = user_info.most_recent_song
+    @messages = Message.custom_display
+    @message = Message.new
   end
 
 
