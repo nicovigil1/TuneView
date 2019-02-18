ca4cf75679f60127f5ed0e0ab0f60c389b24e9bc
Anten Skrabec <cebarks@gmail.com>
2019-02-18 14:24:57 -0700

    fixes Faraday warning

--- app/services/discogs_service.rb
+++ app/services/discogs_service.rb
@@ -2,15 +2,15 @@ class DiscogsService
   def self.conn
     Faraday.new(url: "https://api.discogs.com") do |conn|
       conn.request :url_encoded
-      conn.adapter Faraday.default_adapter
-      conn.response :json, :parser_options => {symbolize_names: true}
       conn.params["key"] = ENV["DISCOGS_KEY"]
       conn.params["secret"] = ENV["DISCOGS_SECRET"]
+      conn.response :json, :parser_options => {symbolize_names: true}
+      conn.adapter Faraday.default_adapter
     end
-  end 
+  end
 
   def self.search(query, type)
     params = "{#{query}}&{?#{type}}&{release}&{?type}"
     conn.get("database/search?q=#{params}")
   end
-end 
+end
