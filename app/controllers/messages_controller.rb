04a3878321c018ff9e420c0b6fa35a3929074a12
SyntheticAutomation <syntheticauto900@gmail.com>
2019-02-18 12:42:44 -0700

    Adds messages controller with private methods

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,23 @@
+class MessagesController < ApplicationController
+
+  def create
+    message = current_user.messages.build(message_params)
+    broadcast(message) if message.save
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
+  def broadcast(message)
+    ActionCable.server.broadcast "chatroom_channel", new_message: render_message(message)
+  end
+
+  def render_message(msg_source)
+    render(partial: 'message', locals: {message: msg_source})
+  end
+
+end
