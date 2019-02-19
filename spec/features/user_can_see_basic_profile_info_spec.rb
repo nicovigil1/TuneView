bae7369dcbe82eb6881297636bcc18b239034613
Anten Skrabec <cebarks@gmail.com>
2019-02-18 18:10:07 -0700

    fixes testing again

--- spec/features/user_can_see_basic_profile_info_spec.rb
+++ spec/features/user_can_see_basic_profile_info_spec.rb
@@ -3,23 +3,20 @@ require 'rails_helper'
 describe "As a user" do
   before(:each) do
     info = {username: "12184696969",
-            image_url: "https://bit.ly/2tlLmZc",
-            spotify_token: ENV["S_TEST_TOKEN"],
-            refresh_token: ENV["REQUEST_TOKEN"],
-            profile_url: "https://open.spotify.com/user/12184696969"}
-    @user = User.create(info)
-    stub_login(@user)
-  end
+      image_url: "https://bit.ly/2tlLmZc",
+      spotify_token: ENV["S_TEST_TOKEN"],
+      refresh_token: ENV["REQUEST_TOKEN"],
+      profile_url: "https://open.spotify.com/user/12184696969"}
+      @user = User.create(info)
+      stub_login(@user)
+    end
 
-  describe "When I visit my dashbaord ('/dashboard')" do
-    it "I should see my spotify username and profile image", :vcr do
-      # require 'pry'; binding.pry
-      visit dashboard_path
+    describe "When I visit my dashbaord ('/dashboard')" do
+      it "I should see my spotify username and profile image", :vcr do
+        visit dashboard_path
 
-      within '#basic-info' do
         expect(page).to have_content("Welcome, #{@user.username}")
         expect(page).to have_css("img[src='#{@user.image_url}']")
       end
     end
   end
-end
