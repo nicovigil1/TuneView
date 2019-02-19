14973611f8aca6a3e64c89f69ba0cf6e6f386e3d
Anten Skrabec <cebarks@gmail.com>
2019-02-18 17:06:45 -0700

    fixes test for profile picture

--- spec/features/user_can_see_basic_profile_info_spec.rb
+++ spec/features/user_can_see_basic_profile_info_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 describe "As a user" do
   before(:each) do
-    @user = create(:user)
+    info = {username: "12184696969",
+            image_url: "https://bit.ly/2tlLmZc",
+            spotify_token: ENV["S_TEST_TOKEN"],
+            refresh_token: ENV["REQUEST_TOKEN"],
+            profile_url: "https://open.spotify.com/user/12184696969"}
+    @user = User.create(info)
     stub_login(@user)
   end
 
   describe "When I visit my dashbaord ('/dashboard')" do
-    it "I should see my spotify username and profile image" do
+    it "I should see my spotify username and profile image", :vcr do
+      # require 'pry'; binding.pry
       visit dashboard_path
 
       within '#basic-info' do
